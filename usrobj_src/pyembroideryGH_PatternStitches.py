37c89c62271f8e3f981192a0696e7925bbbcfca4
fstwn <post@maxeschenbach.com>
2020-08-31 19:27:40 +0200

    revamp PatternStitches component

--- usrobj_src/pyembroideryGH_PatternStitches.py
+++ usrobj_src/pyembroideryGH_PatternStitches.py
@@ -1,105 +1,81 @@
-"""Get all the raw stitches of a pattern. The pattern has to be supplied
+"""
+Get all the raw stitches of a pattern. The pattern has to be supplied
 as an instance of pyembroidery.EmbPattern.
-            Inputs:
-                Pattern: Pattern as pyembroidery EmbPattern instance
-            Output:
-                X: The X-Coordinate of the stitch
-                Y: The Y-Coordinate of the stitch
-                Cmd = The command for the stitch
-                Thread: The thread of the stitch
-                Needle: The needle of the stitch
-                Order: The order of the stitch
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191105"""
+    Inputs:
+        Pattern: Pattern as pyembroidery EmbPattern instance
+    Output:
+        X: The X-Coordinate of the stitch
+        Y: The Y-Coordinate of the stitch
+        Cmd = The command for the stitch
+        Thread: The thread of the stitch
+        Needle: The needle of the stitch
+        Order: The order of the stitch
+    Remarks:
+        Author: Max Eschenbach
+        License: MIT License
+        Version: 200831
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
+# GHENV COMPONENT SETTINGS
 ghenv.Component.Name = "PatternStitches"
 ghenv.Component.NickName = "PSS"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "2 Pattern Analysis"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.05"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
+class PatternStitches(component):
 
     def RunScript(self, Pattern):
         # initialize outputs
-        X = None
-        Y = None
-        Cmd = None
-        Thread = None
-        Needle = None
-        Order = None
+        X = Grasshopper.DataTree[object]()
+        Y = Grasshopper.DataTree[object]()
+        Cmd = Grasshopper.DataTree[object]()
+        Thread = Grasshopper.DataTree[object]()
+        Needle = Grasshopper.DataTree[object]()
+        Order = Grasshopper.DataTree[object]()
 
         # only do something if there is an input to begin with
-        if (Pattern != None and \
-        isinstance(Pattern, Grasshopper.DataTree[object]) == True):
-            # if the tree has more than one path it is a "real" tree
-            if Pattern.BranchCount >= 1:
-                # create GH Tree for all output params
-                X = Grasshopper.DataTree[object]()
-                Y = Grasshopper.DataTree[object]()
-                Cmd = Grasshopper.DataTree[object]()
-                Thread = Grasshopper.DataTree[object]()
-                Needle = Grasshopper.DataTree[object]()
-                Order = Grasshopper.DataTree[object]()
-                # loop through all branches of the tree
-                for i in range(Pattern.BranchCount):
-                    branchList = Pattern.Branch(i)
-                    branchPath = Pattern.Path(i)
-                    # if there is more than one item in the branch
-                    if branchList.Count > 1:
-                        for j in range(branchList.Count):
-                            # make sure supplied pattern is really valid
-                            if not isinstance(branchList[j],
-                                              pyembroidery.EmbPattern):
-                                raise TypeError("The supplied pattern is " + \
-                                                "not a valid pyembroidery." + \
-                                                "EmbPattern instance!")
-                            # Get the stitches of the pattern
-                            stitches = zip(*list(
-                                        branchList[j].get_as_stitches()))
-                            # construct a new path for the outputs
-                            path = list(branchPath)
-                            path.append(j)
-                            path = Grasshopper.Kernel.Data.GH_Path(*path)
-                            # add the relevant data to the output trees
-                            X.AddRange(list([s*0.1 for s in stitches[1]]), path)
-                            Y.AddRange(list([s*0.1 for s in stitches[2]]), path)
-                            Cmd.AddRange(list(stitches[3]), path)
-                            Thread.AddRange(list(stitches[4]), path)
-                            Needle.AddRange(list(stitches[5]), path)
-                            Order.AddRange(list(stitches[6]), path)
-                    # if there is only one item in the branch
-                    elif branchList.Count == 1:
-                        # make sure supplied pattern is really a valid pattern
-                        if not isinstance(branchList[0],
-                                          pyembroidery.EmbPattern):
-                            raise TypeError("The supplied pattern is not a " + \
-                                            "valid pyembroidery.EmbPattern " + \
-                                            "instance!")
-                        # Get the stitches of the pattern
-                        stitches = zip(*list(
-                                    branchList[0].get_as_stitches()))
-                        # add the relevant data to the output trees
-                        X.AddRange(list([s*0.1 for s in stitches[1]]),
-                                        branchPath)
-                        Y.AddRange(list([s*0.1 for s in stitches[2]]),
-                                        branchPath)
-                        Cmd.AddRange(list(stitches[3]), branchPath)
-                        Thread.AddRange(list(stitches[4]), branchPath)
-                        Needle.AddRange(list(stitches[5]), branchPath)
-                        Order.AddRange(list(stitches[6]), branchPath)
+        if Pattern != None:
+            # make sure supplied pattern is really a valid pattern
+            if not isinstance(Pattern, pyembroidery.EmbPattern):
+                rml = self.RuntimeMessageLevel.Error
+                errMsg = ("The supplied pattern is not a valid" +
+                          "pyembroidery.EmbPattern instance!")
+                self.AddRuntimeMessage(rml, errMsg)
+                return (X, Y, Cmd, Thread, Needle, Order)
+            
+            # Get the stitches of the pattern
+            stitches = zip(*list(Pattern.get_as_stitches()))
+            
+            # collect the relevant data and assign it to the outputs
+            X = tuple([s*0.1 for s in stitches[1]])
+            Y = tuple([s*0.1 for s in stitches[2]])
+            Cmd = stitches[3]
+            Thread = stitches[4]
+            Needle = stitches[5]
+            Order = stitches[6]
+        else:
+            rml = self.RuntimeMessageLevel.Warning
+            errMsg = ("Expected EmbPattern, got None!")
+            self.AddRuntimeMessage(rml, errMsg)
 
         # return outputs if you have them; here I try it for you:
         return (X, Y, Cmd, Thread, Needle, Order)
