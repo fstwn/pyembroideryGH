2e1d8a66c5a362e9f910975523d1f3e85ce24608
fstwn <post@maxeschenbach.com>
2020-08-31 19:25:52 +0200

    revamp CommandToString component

--- usrobj_src/pyembroideryGH_CommandToString.py
+++ usrobj_src/pyembroideryGH_CommandToString.py
@@ -1,52 +1,73 @@
-"""Converts any embroidery command from its integer-form into
+"""
+Converts any valid embroidery command from integer into
 its string representation.
-            Inputs:
-                Cmd: The embroidery command as Integer
-            Output:
-                CmdStr: The converted embroidery command as string
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191104"""
+    Inputs:
+        Cmd: The embroidery command as integer.
+             {tree, int}
+    Output:
+        CmdStr: The converted embroidery command as string if the input integer
+                is valid, otherwise Null.
+                {tree, str}
+    Remarks:
+        Author: Max Eschenbach
+        License: MIT License
+        Version: 200831
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
+# GHENV COMPONENT SETTINGS 
 ghenv.Component.Name = "CommandToString"
 ghenv.Component.NickName = "CTS"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "0 Commands"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
-
-    def RunScript(self, Cmd):
+class CommandToString(component):
+    
+    def RunScript(self, CmdTree):
         # initialize output
-        CmdStr = None
+        CmdStr = Grasshopper.DataTree[object]()
+        
         # retrieve common dictionary to translate commands
         common_dict = pyembroidery.get_common_name_dictionary()
-
+        
         # only do something if there is valid input to begin with
-        if (Cmd != None and \
-        isinstance(Cmd, Grasshopper.DataTree[object]) == True):
-            # initialize output as GH tree
-            CmdStr = Grasshopper.DataTree[object]()
+        if CmdTree != None:
             # loop through all branches of the tree
-            for i in range(Cmd.BranchCount):
-                branchList = Cmd.Branch(i)
-                branchPath = Cmd.Path(i)
+            for i, branch in enumerate(CmdTree.Branches):
+                branch_path = CmdTree.Path(i)
                 # loop through all items in the current branch
-                for j in range(branchList.Count):
+                for j, command in enumerate(branch):
                     # look up the command in the dict and add the result to
                     # the output tree
-                    CmdStr.Add(common_dict.get(branchList[j]), branchPath)
-
+                    try:
+                        CmdStr.Add(common_dict[command], branch_path)
+                    except KeyError:
+                        rml = self.RuntimeMessageLevel.Warning
+                        errMsg = ("Command '{}' at branch {}, index {} " +
+                                  "is not a known embroidery command integer!" +
+                                  " A Null item will be inserted into the " +
+                                  "output tree!")
+                        errMsg = errMsg.format(command, i, j)
+                        self.AddRuntimeMessage(rml, errMsg)
+                        CmdStr.Add(None, branch_path)
+        
         # return outputs if you have them; here I try it for you:
         return CmdStr
