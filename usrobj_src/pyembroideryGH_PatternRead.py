adec123a1ec276efe80dabea9997f2a1c4629f27
fstwn <post@maxeschenbach.com>
2020-08-31 19:26:58 +0200

    revamp PatternRead component

--- usrobj_src/pyembroideryGH_PatternRead.py
+++ usrobj_src/pyembroideryGH_PatternRead.py
@@ -1,49 +1,60 @@
-"""Reads any readable embroidery format and returns the
+"""
+Reads any readable embroidery format and returns the
 pattern as an instance of pyembroidery.EmbPattern
     Inputs:
-        FilePath: Filepath of the embroidery pattern file
+        FilePath: Filepath of the embroidery pattern file.
+                  {item, path}
     Output:
-        Pattern: The embroidery pattern as pyembroidery.EmbPattern instance"""
+        Pattern: The embroidery pattern as pyembroidery.EmbPattern instance.
+                 {item/list/tree, EmbPattern}
+    Remarks:
+        Author: Max Eschenbach
+        License: MIT License
+        Version: 200831
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+from os import path
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
+# GHENV COMPONENT SETTINGS
 ghenv.Component.Name = "PatternRead"
 ghenv.Component.NickName = "PR"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "1 Input/Output"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
+class PatternRead(component):
 
     def RunScript(self, FilePath):
         # Initialize output
-        Pattern = None
-
+        Pattern = Grasshopper.DataTree[object]()
+        
         # only do something if input is defined
-        if (FilePath != None and \
-        isinstance(FilePath, Grasshopper.DataTree[object]) == True):
-            # create empty output tree
-            Pattern = Grasshopper.DataTree[object]()
-            # loop over all branches
-            for i in range(FilePath.BranchCount):
-                branchList = FilePath.Branch(i)
-                branchPath = FilePath.Path(i)
-                # loop over all items in the current branch
-                for j in range(branchList.Count):
-                    try:
-                        ptrn = pyembroidery.read(branchList[j])
-                        Pattern.Add(ptrn, branchPath)
-                    except:
-                        raise RuntimeError("Could not read embroidery file!" + \
-                                          "Please check if format is readable.")
-
+        if FilePath != None:
+            try:
+                Pattern = pyembroidery.read(path.normpath(
+                                                    FilePath.strip("\n\r")))
+            except Exception as e:
+                rml = self.RuntimeMessageLevel.Error
+                errMsg = ("Could not read embroidery file!" +
+                          "Please check if format is readable.")
+                self.AddRuntimeMessage(rml, errMsg)
+        
         # return outputs if you have them; here I try it for you:
         return Pattern
