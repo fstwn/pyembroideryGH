1c612c2be4a22422427ac2006526d0c2f03b4c94
fstwn <post@maxeschenbach.com>
2020-08-31 19:26:24 +0200

    revamp DeconstructThread component

--- usrobj_src/pyembroideryGH_DeconstructThread.py
+++ usrobj_src/pyembroideryGH_DeconstructThread.py
@@ -1,87 +1,106 @@
-"""Deconstructs a thread by retrieving all of its relevant data.
+"""
+Deconstructs a thread by retrieving all of its relevant data.
 The thread has to be supplied as an instance of pyembroidery.EmbThread.
-            Inputs:
-                Thread: The thread of a stitch/pattern as
-                        pyembroidery.EmbThread instance
-            Output:
-                Color: The color of the thread as pyembroidery color string
-                ColorRGB: The color of the thread as System.Color RGB Color
-                Description: The description of the thread
-                CatalogNr: The catalogue number of the thread
-                Details: The details of the thread
-                Brand: The brand of the thread
-                Chart: The chart of the thread
-                Weight: The weight of the thread
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191104"""
+    Inputs:
+        Thread: The thread of a stitch/pattern as an instance of
+                pyembroidery.EmbThread.
+                {tree, EmbThread}
+    Output:
+        ColorRGB: The color of the thread as a System.Drawing.Color RGB Color.
+                  {tree, System.Drawing.Color}
+        Description: The color of the thread as pyembroidery color string.
+                     {tree, str}
+        CatalogNr: The catalogue number of the thread.
+                   {tree, int}
+        Details: The details of the thread if present, otherwise Null.
+                 {tree, ?}
+        Brand: The brand of the thread if present, othwerise Null.
+               {tree, str}
+        Chart: The chart of the thread if present, otherwise Null.
+               {tree, string}
+        Weight: The weight of the thread if present, otherwise Null.
+                {tree, ?}
+    Remarks:
+        Author: Max Eschenbach
+        License: MIT License
+        Version: 200831
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import System.Drawing.Color as SysColor
-import pyembroidery
 
+# THIRD PARTY MODULE IMPORTS
+import System.Drawing.Color as SysColor
+
+# GHENV COMPONENT SETTINGS 
 ghenv.Component.Name = "DeconstructThread"
 ghenv.Component.NickName = "DT"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "2 Pattern Analysis"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
-
-    def RunScript(self, Thread):
-        # initialize outputs
-        Color = None
-        ColorRGB = None
-        Description = None
-        CatalogNr = None
-        Details = None
-        Brand = None
-        Chart = None
-        Weight = None
-
+class DeconstructThread(component):
+    
+    def RunScript(self, ThreadTree):
+        # initialize ouputs as DataTrees
+        ColorRGB = Grasshopper.DataTree[object]()
+        Description = Grasshopper.DataTree[object]()
+        CatalogNr = Grasshopper.DataTree[object]()
+        Details = Grasshopper.DataTree[object]()
+        Brand = Grasshopper.DataTree[object]()
+        Chart = Grasshopper.DataTree[object]()
+        Weight = Grasshopper.DataTree[object]()
+        
         # only execute if there is a thread supplied to begin with
-        if (Thread != None and \
-        isinstance(Thread, Grasshopper.DataTree[object]) == True):
-            # if the tree has more than one path it is a "real" tree
-            if len(list(Thread.Paths)) >= 1:
-                # initialize ouputs as DataTrees
-                Color = Grasshopper.DataTree[object]()
-                ColorRGB = Grasshopper.DataTree[object]()
-                Description = Grasshopper.DataTree[object]()
-                CatalogNr = Grasshopper.DataTree[object]()
-                Details = Grasshopper.DataTree[object]()
-                Brand = Grasshopper.DataTree[object]()
-                Chart = Grasshopper.DataTree[object]()
-                Weight = Grasshopper.DataTree[object]()
-
-               # loop over branches of original tree
-                for i in range(Thread.BranchCount):
-                    branchList = Thread.Branch(i)
-                    branchPath = Thread.Path(i)
-
-                    # loop over all items of original tree,
-                    # extract data and add to output trees
-                    for j in range(branchList.Count):
-                        Color.Add(branchList[j].color, branchPath)
+        if ThreadTree != None:
+           # loop over branches of the thread tree
+            for i, branch in enumerate(ThreadTree.Branches):
+                branch_path = ThreadTree.Path(i)
+                
+                # loop over all items in branch
+                for j, thread in enumerate(branch):
+                    if isinstance(thread, pyembroidery.EmbThread):
                         ColorRGB.Add(SysColor.FromArgb(255,
-                                                      branchList[j].get_red(),
-                                                      branchList[j].get_green(),
-                                                      branchList[j].get_blue()),
-                                                      Thread.Path(i))
-                        Description.Add(branchList[j].description, branchPath)
-                        CatalogNr.Add(branchList[j].catalog_number, branchPath)
-                        Details.Add(branchList[j].details, branchPath)
-                        Brand.Add(branchList[j].brand, branchPath)
-                        Chart.Add(branchList[j].chart, branchPath)
-                        Weight.Add(branchList[j].weight, branchPath)
-
+                                                      thread.get_red(),
+                                                      thread.get_green(),
+                                                      thread.get_blue()),
+                                                      branch_path)
+                        Description.Add(thread.description, branch_path)
+                        CatalogNr.Add(thread.catalog_number, branch_path)
+                        Details.Add(thread.details, branch_path)
+                        Brand.Add(thread.brand, branch_path)
+                        Chart.Add(thread.chart, branch_path)
+                        Weight.Add(thread.weight, branch_path)
+                    else:
+                        rml = self.RuntimeMessageLevel.Warning
+                        errMsg = ("Thread '{}' at branch {}, index {} " +
+                                  "is not a valid instance of " +
+                                  "pyembroidery.EmbThread! A Null item will " +
+                                  "be inserted into the output tree!")
+                        errMsg = errMsg.format(thread, i, j)
+                        self.AddRuntimeMessage(rml, errMsg)
+                        ColorRGB.Add(None, branch_path)
+                        Description.Add(None, branch_path)
+                        CatalogNr.Add(None, branch_path)
+                        Details.Add(None, branch_path)
+                        Brand.Add(None, branch_path)
+                        Chart.Add(None, branch_path)
+                        Weight.Add(None, branch_path)
+        
         # return outputs if you have them; here I try it for you:
-        return (ColorRGB, Description, CatalogNr, Details, Brand, Chart, Weight)
+        return ColorRGB, Description, CatalogNr, Details, Brand, Chart, Weight
