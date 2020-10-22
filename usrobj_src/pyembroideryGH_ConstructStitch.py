09a719016e6dcc0e20420240d1e318d4db600b58
fstwn <post@maxeschenbach.com>
2020-10-22 18:16:02 +0200

    update docstring and simplify logic

--- usrobj_src/pyembroideryGH_ConstructStitch.py
+++ usrobj_src/pyembroideryGH_ConstructStitch.py
@@ -1,126 +1,83 @@
-"""Constructs a Stitch-String in the format "PtX,PtY,Cmd"
-            Inputs:
-                Pt: The input Point(s)
-                Cmd: The corresponding Command for each point coordinate
-            Output:
-                Stitch: The data for the stitch as String
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191104"""
+"""
+Constructs a Stitch-String in the format (PtX, PtY, Cmd) by pairing each input
+coordinate with an input command.
+    Inputs:
+        Pt: The input coordinate (point) for the stitch.
+            {list, point3d}
+        Cmd: The corresponding command integer for each point coordinate.
+             {list, int}
+    Output:
+        Stitch: The constructed stitch as String.
+                {item/list/tree, str}
+    Remarks:
+        Author: Max Eschenbach
+        License: MIT License
+        Version: 201022
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
+# GHENV COMPONENT SETTINGS
 ghenv.Component.Name = "ConstructStitch"
 ghenv.Component.NickName = "CS"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "3 Pattern Creation"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
-
-class MyComponent(component):
+class ConstructStitch(component):
 
     def RunScript(self, Pt, Cmd):
         # initialize ouputs
-        Stitch = None
-
-        # only do something if there is data to begin with
-        if (Pt != None and Cmd != None and \
-        isinstance(Pt, Grasshopper.DataTree[object]) == True and \
-        isinstance(Cmd, Grasshopper.DataTree[object]) == True):
-            # case: both inputs as trees with more than one path
-            if Pt.BranchCount > 1 and Cmd.BranchCount > 1:
-                # subcase: both BranchCount and number of items are identical
-                pt_ipb = [len(br) for br in Pt.Branches]
-                cmd_ipb = [len(br) for br in Cmd.Branches]
-                if pt_ipb == cmd_ipb:
-                    Stitch = Grasshopper.DataTree[object]()
-                    for i in range(Pt.BranchCount):
-                        branchList = Pt.Branch(i)
-                        branchPath = Pt.Path(i)
-                        for j in range(branchList.Count):
-                            pt = list(branchList[j])
-                            comm = Cmd.Branch(i)[j]
-                            stitch_str = (",".join([str(pt[0]*10),
-                                            str(pt[1]*10), str(comm)]))
-                            Stitch.Add(stitch_str, branchPath)
-                # subcase: branchcount and number of items are not identical
-                elif pt_ipb != cmd_ipb:
-                    # sub-subcase: only branchcount is identical
-                    if (Pt.BranchCount == Cmd.BranchCount and \
-                    cmd_ipb == [1 for b in Cmd.Branches]):
-                        Stitch = Grasshopper.DataTree[object]()
-                        for i in range(Pt.BranchCount):
-                            branchList = Pt.Branch(i)
-                            branchPath = Pt.Path(i)
-                            for j in range(branchList.Count):
-                                pt = list(branchList[j])
-                                comm = Cmd.Branch(i)[0]
-                                stitch_str = (",".join([str(pt[0]*10),
-                                                str(pt[1]*10), str(comm)]))
-                                Stitch.Add(stitch_str, branchPath)
-                    # both branchcount and items per branch differ completely
+        Stitches = []
+        
+        # only act if there is some data to begin with
+        if Pt and Cmd:
+            # list of points is longer or equal to list of commands, so we
+            # loop over the list of points
+            if len(Pt) >= len(Cmd):
+                for i, pt in enumerate(Pt):
+                    # extract command with failsafe
+                    if i <= len(Cmd) - 1:
+                        command = Cmd[i]
                     else:
-                        raise NotImplementedError("Combining a tree of " + \
-                            "points with a tree of commands that is " + \
-                            "fundamentally different in structure " + \
-                            "is not implemented yet! Still thinking of a " + \
-                            "useful way to handle this...")
-            # case: tree of points and flat list or single item
-            elif Pt.BranchCount > 1 and Cmd.BranchCount == 1:
-                # subcase: cmd is a flat list
-                # behaviour: thinking of something useful...
-                if Cmd.Branch(0).Count > 1:
-                    raise NotImplementedError("The combination of a tree " + \
-                            "of points and flat list of commands has not " + \
-                            "been implemented yet! Still thinking of a " + \
-                            "useful way to handle this...")
-                # subcase: cmd is one single item
-                # behaviour: cmd is applied to the whole tree of stitch-points
-                elif Cmd.Branch(0).Count == 1:
-                    Stitch = Grasshopper.DataTree[object]()
-                    for i in range(Pt.BranchCount):
-                        branchList = Pt.Branch(i)
-                        branchPath = Pt.Path(i)
-                        for j in range(branchList.Count):
-                            pt = list(branchList[j])
-                            comm = Cmd.Branch(0)[0]
-                            stitch_str = (",".join([str(pt[0]*10),
-                                              str(pt[1]*10), str(comm)]))
-                            Stitch.Add(stitch_str, branchPath)
-            # case: both inputs are flat
-            elif Pt.BranchCount == 1 and Cmd.BranchCount == 1:
-                # subcase: both BranchCount and number of items are identical
-                pt_ipb = [len(br) for br in Pt.Branches]
-                cmd_ipb = [len(br) for br in Cmd.Branches]
-                if pt_ipb == cmd_ipb:
-                    Stitch = Grasshopper.DataTree[object]()
-                    for i in range(Pt.BranchCount):
-                        branchList = Pt.Branch(i)
-                        branchPath = Pt.Path(i)
-                        for j in range(branchList.Count):
-                            pt = list(branchList[j])
-                            comm = Cmd.Branch(i)[j]
-                            stitch_str = (",".join([str(pt[0]*10),
-                                            str(pt[1]*10), str(comm)]))
-                            Stitch.Add(stitch_str, branchPath)
-                elif pt_ipb != cmd_ipb and cmd_ipb == [1]:
-                    Stitch = Grasshopper.DataTree[object]()
-                    for i in range(Pt.BranchCount):
-                        branchList = Pt.Branch(i)
-                        branchPath = Pt.Path(i)
-                        for j in range(branchList.Count):
-                            pt = list(branchList[j])
-                            comm = Cmd.Branch(0)[0]
-                            stitch_str = (",".join([str(pt[0]*10),
-                                              str(pt[1]*10), str(comm)]))
-                            Stitch.Add(stitch_str, branchPath)
+                        command = Cmd[-1]
+                    # compile stitch string
+                    stitch_str = ",".join([str(pt.X * 10), 
+                                           str(pt.Y * -10),
+                                           str(command)])
+                    # append to list of stitches
+                    Stitches.append(stitch_str)
+            # list of commands is longer than list of points, so we have to
+            # loop over the list of commands
+            elif len(Pt) < len(Cmd):
+                for j, command in enumerate(Cmd):
+                    # extract coordinate with failsafe
+                    if j <= len(Pt) - 1:
+                        pt = Pt[j]
+                    else:
+                        pt = Pt[-1]
+                    # compile stitch string
+                    stitch_str = ",".join([str(pt.X * 10),
+                                           str(pt.Y * -10),
+                                           str(command)])
+                    # append to list of stitches
+                    Stitches.append(stitch_str)
+        else:
+            rml = self.RuntimeMessageLevel.Warning
+            if not Pt:
+                errMsg = "Input Pt failed to collect data!"
+                self.AddRuntimeMessage(rml, errMsg)
+            if not Cmd:
+                errMsg = "Input Cmd failed to collect data!"
+                self.AddRuntimeMessage(rml, errMsg)
+            return Grasshopper.DataTree[object]()
+        
         # return outputs if you have them; here I try it for you:
-        return Stitch
+        return Stitches
