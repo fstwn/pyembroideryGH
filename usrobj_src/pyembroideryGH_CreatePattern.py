377ad9bd734ebc4c5bbbd76793cff9af45d6f4b0
fstwn <post@maxeschenbach.com>
2020-10-22 18:20:21 +0200

    update and simplify logic

--- usrobj_src/pyembroideryGH_CreatePattern.py
+++ usrobj_src/pyembroideryGH_CreatePattern.py
@@ -1,52 +1,57 @@
-"""Create one or a collection of empty embroidery patterns as
-instances of pyembroidery.EmbPattern
-            Inputs:
-                N: The number of empty patterns to be created, 1 if left blank
-            Output:
-                PatternOut: The empty pyembroidery.EmbPattern instance
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191104"""
+"""
+Create one or a collection of empty embroidery patterns as
+instances of pyembroidery.EmbPattern.
+    Inputs:
+        N: The number of empty patterns to be created, 1 if left blank.
+           {item, int}
+    Output:
+        Pattern: The empty pattern as a pyembroidery.EmbPattern instance.
+                 {item/list/tree, EmbPattern}
+    Remarks:
+        Author: Max Eschenbach
+        License: MIT License
+        Version: 201022
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
+# GHENV COMPONENT SETTINGS
 ghenv.Component.Name = "CreatePattern"
 ghenv.Component.NickName = "CP"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "3 Pattern Creation"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
+class CreatePattern(component):
 
     def RunScript(self, N):
         # default for N
-        if not N or N == None or N.DataCount == 0:
-            N.Add(1)
-
+        if N is None:
+            N = 1
+        
         # initialize output
-        PatternOut = Grasshopper.DataTree[object]()
-
-        # loop through all branches of the tree
-        for i in range(N.BranchCount):
-            branchList = N.Branch(i)
-            branchPath = N.Path(i)
-
-            # loop through all items in the current branch
-            for j in range(branchList.Count):
-                num_pats = branchList[j]
-                # create empty patterns
-                for x in range(num_pats):
-                    pat = pyembroidery.EmbPattern()
-                    PatternOut.Add(pat, branchPath)
-
+        Pattern = System.Collections.Generic.List[object]()
+        
+        # create empty patterns
+        for i in range(N):
+            pat = pyembroidery.EmbPattern()
+            Pattern.Add(pat)
+        
         # return outputs if you have them; here I try it for you:
-        return PatternOut
+        return Pattern
