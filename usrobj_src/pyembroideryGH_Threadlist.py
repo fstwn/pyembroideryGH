ab2c5e33777ba06ab90cd85c7ede78259f666513
fstwn <post@maxeschenbach.com>
2020-10-30 18:04:29 +0100

    update logic and add runtime message

--- usrobj_src/pyembroideryGH_Threadlist.py
+++ usrobj_src/pyembroideryGH_Threadlist.py
@@ -1,76 +1,62 @@
-"""Gets the threads of one or many input patterns
-            Inputs:
-                Pattern: The embroidery pattern
-                as pyembroidery.EmbPattern instance
-            Output:
-                Thread: The threads of the pattern as
-                        pyembroidery.EmbThread instance
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191104"""
+"""
+Gets the threads of one or many input patterns
+    Inputs:
+        Pattern: The embroidery pattern as
+                 pyembroidery.EmbPattern instance.
+                 {item, EmbPattern}
+    Output:
+        Thread: The threads of the pattern as
+                pyembroidery.EmbThread instance.
+                {item/list/tree, EmbThread}
+    Remarks:
+        Author: Max Eschenbach
+        License: Apache License 2.0
+        Version: 201030
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
-ghenv.Component.Name = "Threadlist"
+# GHENV COMPONENT SETTINGS
+ghenv.Component.Name = "ThreadList"
 ghenv.Component.NickName = "TL"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "2 Pattern Analysis"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
+class ThreadList(component):
 
     def RunScript(self, Pattern):
         # initialize output tree
         Thread = Grasshopper.DataTree[object]()
-
-        # loop through all the branches in the pattern-tree
-        for i in range(Pattern.BranchCount):
-            branchList = Pattern.Branch(i)
-            branchPath = Pattern.Path(i)
-
-            # loop through all the items in the current branch
-            # of the pattern tree
-            for j in range(branchList.Count):
-                # if there is only one pattern in the branch
-                if branchList.Count == 1:
-                    ptrn = branchList[j]
-                    # check if pattern is really a valid embroidery pattern
-                    if isinstance(ptrn, pyembroidery.EmbPattern) == True:
-                        # get the threadlist of the pattern
-                        threadList = ptrn.threadlist
-                        # add all the threads to the output tree
-                        for t in threadList:
-                            Thread.Add(t, branchPath)
-                    else:
-                        raise TypeError("The supplied pattern is " + \
-                                        "not a valid pyembroidery." + \
-                                        "EmbPattern instance!")
-                # if there is more than one pattern in the branch
-                elif branchList.Count > 1:
-                    ptrn = branchList[j]
-                    # check if pattern is really a valid embroidery pattern
-                    if isinstance(ptrn, pyembroidery.EmbPattern) == True:
-                        # get the threadlist of the pattern
-                        threadList = ptrn.threadlist
-                        # modify the path for output
-                        newPath = list(branchPath)
-                        newPath.append(j)
-                        newPath = Grasshopper.Kernel.Data.GH_Path(*newPath)
-                        # add all the threads to the output tree
-                        for t in threadList:
-                            Thread.Add(t, newPath)
-                    else:
-                        raise TypeError("The supplied pattern is " + \
-                                        "not a valid pyembroidery." + \
-                                        "EmbPattern instance!")
+        
+        # make sure supplied pattern is really a valid pattern
+        if Pattern is not None:
+            if not isinstance(Pattern, pyembroidery.EmbPattern):
+                raise TypeError("The supplied pattern is not a valid " +
+                                "pyembroidery.EmbPattern instance!")
+            else:
+                # get the threadlist of the pattern
+                Thread = Pattern.threadlist
+        else:
+            rml = self.RuntimeMessageLevel.Warning
+            errMsg = ("Input Pattern failed to collect data!")
+            self.AddRuntimeMessage(rml, errMsg)
+        
         # return outputs if you have them; here I try it for you:
         return Thread
