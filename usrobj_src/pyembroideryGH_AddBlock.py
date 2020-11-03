7d575c3a9d739b53e0b5553ca0b2e4f6644883bb
fstwn <post@maxeschenbach.com>
2020-11-03 15:02:56 +0100

    update addblokc component

--- usrobj_src/pyembroideryGH_AddBlock.py
+++ usrobj_src/pyembroideryGH_AddBlock.py
@@ -1,101 +1,103 @@
-"""Adds a block of stitches to an embroidery pattern supplied as
+"""
+Adds a block of stitches to an embroidery pattern supplied as
 pyembroidery.EmbPattern instance
-            Inputs:
-                Pattern: The pattern to be modified as
-                pyembroidery.EmbPattern instance. This works only on a single
-                pattern for now!
-                Stitch: The block(s) of stitches to add to the pattern. If a
-                tree is supplied, each branch will be treated as one block.
-                Thread: The threads for the pattern, corresponding to
-                the blocks of stitches. The paths of the block and the thread
-                have to correspond, otherwise the thread won't be found.
-                If there's more than one thread in the branch, only the first
-                thread will be used!
-            Output:
-                PatternOut: The modified pattern with the stitches added
-            Remarks:
-                Author: Max Eschenbach
-                License: Apache License 2.0
-                Version: 191104"""
+    Inputs:
+        Pattern: The pattern to be modified as
+                 pyembroidery.EmbPattern instance.
+                 {item, EmbPattern}
+        Stitches: The block(s) of stitches to add to the pattern. If a
+                  tree is supplied, each branch will be treated as one block.
+                  {list, str}
+        Thread: The threads for the pattern, corresponding to
+                the blocks of stitches. This is optional, If no thread is
+                supplied, the blocks will still be added to the pattern!
+                {item, EmbThread}
+    Output:
+        Pattern: The modified pattern with the blocks of stitches and the their
+                 thread added.
+                 {item/list/tree, EmbPattern}
+    Remarks:
+        Author: Max Eschenbach
+        License: Apache License 2.0
+        Version: 201103
+"""
 
+# PYTHON STANDARD LIBRARY IMPORTS
 from __future__ import division
+import re
+
+# GHPYTHON SDK IMPORTS
 from ghpythonlib.componentbase import executingcomponent as component
 import Grasshopper, GhPython
 import System
 import Rhino
 import rhinoscriptsyntax as rs
-import pyembroidery
 
+# GHENV COMPONENT SETTINGS
 ghenv.Component.Name = "AddBlock"
 ghenv.Component.NickName = "AB"
 ghenv.Component.Category = "pyembroideryGH"
 ghenv.Component.SubCategory = "3 Pattern Creation"
 
-__author__ = "Max Eschenbach"
-__version__ = "2019.11.04"
+# LOCAL MODULE IMPORTS
+try:
+    import pyembroidery
+except ImportError:
+    errMsg = ("The pyembroidery python module seems to be not correctly " +
+              "installed! Please make sure the module is in you search " +
+              "path, see README for instructions!.")
+    raise ImportError(errMsg)
 
-class MyComponent(component):
+class AddBlock(component):
 
-    def RunScript(self, Pattern, Stitch, Thread):
-        # initialize outputs
-        PatternOut = Grasshopper.DataTree[object]()
-
-        # copy the input pattern to avoid modification on the original object
-        if isinstance(Pattern, pyembroidery.EmbPattern) == True:
-            cPattern = Pattern.copy()
-        else:
-            raise TypeError("Supplied pattern is no valid " + \
-                            "pyembroidery.EmbPattern instance! " + \
-                            "Please check your inputs and try again.")
-
-        stitches = []
-        blocks = []
-        # loop over all branches of the stitch tree
-        for i in range(Stitch.BranchCount):
-            branchList = Stitch.Branch(i)
-            branchPath = Stitch.Path(i)
-            # define list for block
-            block = []
-            ptBlock = []
-            # loop over all items in the current branch of stitches
-            for j in range(branchList.Count):
-                # convert the stitch
-                stitch = branchList[j].split(",")
-                stitch = [float(stitch[0]), float(stitch[1]), int(stitch[2])]
-                block.append(stitch)
-
-                # append to list for preview
-                ptSt = [stitch[0]*0.1, stitch[1]*0.1, stitch[2]]
-                stitches.append(ptSt)
-                ptBlock.append(ptSt)
-            blocks.append(ptBlock)
-            # check for corresponding threads for the block of stitches
-            if Thread.PathExists(branchPath):
-                try:
-                    # get the list of items in the branchpath
-                    tList = list(Thread.Branch(branchPath))
-                    thread = tList[0]
-                    # ensure that threads are real threads
-                    if isinstance(thread, \
-                    pyembroidery.EmbThread.EmbThread) != True:
-                        raise TypeError("Supplied threads are no valid " + \
-                                        "pyembroidery.EmbThread instances! " + \
-                                        "Please check your inputs and try " + \
-                                        "again.")
-                except IndexError:
-                    thread = None
+    def RunScript(self, input_pattern, Stitches, Thread):
+        # initialize output
+        Pattern = Grasshopper.DataTree[object]()
+        
+        # check if valid data is coming in
+        if input_pattern != None and Stitches:
+            # check if pattern is valid
+            if not isinstance(input_pattern, pyembroidery.EmbPattern):
+                raise TypeError("The supplied pattern is not a valid " +
+                                "pyembroidery.EmbPattern instance!")
             else:
-                thread = None
-
-            # add the block with corresponding thread to the pattern
-            cPattern.add_block(block, thread)
-
-        # define outputs
-        PatternOut.Add(cPattern)
-
-        # add to document for preview
-        #stitches = [rs.AddPoint(pt) for pt in stitches]
-        #polys = [rs.AddPolyline(polypts) for polypts in blocks]
-
+                # copy input pattern to avoid changing the original
+                Pattern = input_pattern.copy()
+                # regex pattern for matching stitch strings
+                regex = re.compile(r'^([+-]?(\d+([.]\d*)?([eE][+-]?\d+)?|[.]\d+([eE][+-]?\d+)?)[,]){2}[-+]?[0-9]+$')
+                # check and extract stitches, compile valid_stitches
+                valid_stitches = []
+                for i, stitch in enumerate(Stitches):
+                    if bool(regex.match(stitch)):
+                        # convert the stitch
+                        stitch = stitch.split(",")
+                        stitch = (float(stitch[0]),
+                                  float(stitch[1]),
+                                  int(stitch[2]))
+                        valid_stitches.append(stitch)
+                    else:
+                        rml = self.RuntimeMessageLevel.Warning
+                        errMsg = ("{}. stitch at index {} is not a " +
+                                  "valid stitch string! Skipping this stitch!")
+                        errMsg = errMsg.format(i, self.RunCount)
+                        self.AddRuntimeMessage(rml, errMsg)
+                if Thread != None:
+                    if not isinstance(Thread, pyembroidery.EmbThread):
+                        raise TypeError("The supplied thread is not a valid " +
+                                        "pyembroidery.EmbThread instance!")
+                else:
+                    rml = self.RuntimeMessageLevel.Remark
+                    errMsg = ("No Thread supplied. Resulting block will not " +
+                              "have a thread attached.")
+                    errMsg = errMsg.format(self.RunCount)
+                    self.AddRuntimeMessage(rml, errMsg)
+                # add the block and thread to the pattern
+                Pattern.add_block(valid_stitches, Thread)
+        else:
+            rml = self.RuntimeMessageLevel.Warning
+            if not Stitches:
+                errMsg = ("Input Stitches failed to collect data!")
+                self.AddRuntimeMessage(rml, errMsg)
+        
         # return outputs if you have them; here I try it for you:
-        return PatternOut
+        return Pattern
